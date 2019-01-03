362a1b22c91b13e5ee05fb365decbb4c741f7b18
Wesley Petrowski <WesleyP@iqmetrix.com>
2019-01-03 11:43:21 -0600

    Add project files.

---
+++ DecorationIssue/QueryHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DecorationIssue
+{
+    public interface IQueryHandler<T>
+    {
+        T Handle();
+    }
+
+    public class GetIntQueryHandler : IQueryHandler<int>
+    {
+        public int Handle()
+        {
+            Console.WriteLine("int");
+            return 0;
+        }
+    }
+
+    public class GetDoubleQueryHandler : IQueryHandler<double>
+    {
+        public double Handle()
+        {
+            Console.WriteLine("double");
+            return 0.0;
+        }
+    }
+
+    public interface IMySpecialHandler : IQueryHandler<string> { }
+
+    public class MySpecialHandler : IMySpecialHandler
+    {
+        public string Handle()
+        {
+            Console.WriteLine("string");
+            return "";
+        }
+    }
+
+    public class Decorator<T> : IQueryHandler<T>
+    {
+        private readonly IQueryHandler<T> _inner;
+
+        public Decorator(IQueryHandler<T> inner)
+        {
+            _inner = inner;
+        }
+
+        public T Handle()
+        {
+            Console.WriteLine("string");
+            return _inner.Handle();
+        }
+    }
+}
---
+++ DecorationIssue/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DecorationIssue
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.Scan(scan => scan
+                .FromApplicationDependencies()
+                    .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<>)).Where(type => type != typeof(Decorator<>)))
+                        .AsImplementedInterfaces());
+
+            services.Decorate(typeof(IQueryHandler<>), typeof(Decorator<>));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
